f6e92b230ebd39aac3bf34df1c743bcc015fdbae
Ubuntu <ubuntu@teste.mpir5ivhmcgejjrpy4j2ch0j4a.nx.internal.cloudapp.net>
2021-12-14 20:43:43 +0000

    Creating hex from Pcap sample

--- pooling.py
+++ pooling.py
@@ -1,9 +1,15 @@
 import os
 import sys
+from scapy.all import *
+
 
 def runner(pkt_amount, duration, interface):
     cmd = 'sudo dumpcap -i '+str(interface)+' -c '+str(pkt_amount)+' -a duration:'+str(duration)+ ' -w /tmp/output.pcap'
     os.system(cmd)
 
 if __name__ == "__main__":
-   runner(sys.argv[1], sys.argv[2], sys.argv[3] )
+   runner(sys.argv[1], sys.argv[2], sys.argv[3])
+   packets = rdpcap('output.pcap')
+   for i in range(len(packets)):
+    print(raw(packets[i]))
+
