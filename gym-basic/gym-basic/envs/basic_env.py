50c2e37d5022b91d93124c30a335481f55100417
Ubuntu <ubuntu@ste.oc33sdq0deuele5z02z3wf4cwh.dx.internal.cloudapp.net>
2021-12-16 20:08:44 +0000

    Customized Scenario almost done for Reinforcement Learning

--- gym-basic/gym-basic/envs/basic_env.py
+++ gym-basic/gym-basic/envs/basic_env.py
@@ -1,7 +1,16 @@
+'''
+Auhor: Rodrigo Moreira rodrigo at ufv dot br
+Based on: https://towardsdatascience.com/beginners-guide-to-custom-environments-in-openai-s-gym-989371673952
+'''
+
+
 import gym
 from stable_baselines.common.env_checker import check_env
+import numpy as np
 
-env = gym.make("gym_basic:basic-v0")
+from stable_baselines.common.policies import MlpPolicy
+from stable_baselines.common.vec_env import DummyVecEnv
+from stable_baselines import PPO2
 
 class BasicEnv(gym.Env):
 
@@ -25,4 +34,26 @@ class BasicEnv(gym.Env):
             state = 0
             return state
 
+    def render(slef, mode='human'):
+            pass
+
+#env = gym.make("gym_basic:basic-v0")
+env = BasicEnv()
 check_env(env)
+
+action_space_size = env.action_space.n
+state_space_size = env.observation_space.n
+q_table = np.zeros((state_space_size, action_space_size))
+print(q_table)
+
+
+model = PPO2(MlpPolicy, env, verbose=1)
+model.learn(total_timesteps=10000)
+
+obs = env.reset()
+
+for i in range(10):
+    action, _states = model.predict(obs)
+    print(action)
+    obs, rewards, dones, info = env.step(action)
+    env.render()
